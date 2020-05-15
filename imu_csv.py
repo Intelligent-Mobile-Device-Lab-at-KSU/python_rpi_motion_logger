3f1246b03811c8d1e4038d95d633d1e3aaedbf3d
Daniel Castle <dcastle4@students.kennesaw.edu>
2020-05-15 16:04:30 +0100

    Cleaned up script

--- imu_csv.py
+++ imu_csv.py
@@ -10,9 +10,7 @@ from sys import exit
 from sense_hat import SenseHat
 from csv import writer
 from datetime import datetime
-
-record_toggle = True
-current_mode = "idle"
+from sys import argv
 
 # color definitions for SenseHat 8x8 RGB LED array
 red	 = (255,0,0)
@@ -23,20 +21,9 @@ pink	 = (255,0,255)
 yellow	 = (255,255,0)
 black	 = (0,0,0)
 
-
-def record_color(record_toggle):
-
-	# if recording, make the LED color red (like a camcorder)
-	if record_toggle == True:
-		return red
-	# make the LED color green if not recording
-	else:
-		return green
-
-
 # lock pick mode pattern for 8x8 RGB LED array
 pick_led_pattern = [
-	black, blue, black, black, cyan, cyan, cyan, record_color(record_toggle),	# first row
+	black, blue, black, black, cyan, cyan, cyan, black,				# first row
 	blue, black, blue, black, black, cyan, black, black,				# second row
 	blue, blue, black, black, black, cyan, black, black,				# third row
 	blue, black, black, black, cyan, cyan, cyan, black,				# fourth row
@@ -48,7 +35,7 @@ pick_led_pattern = [
 
 # idle mode pattern for 8x8 RGB LED array
 idle_led_pattern = [
-	blue, blue, blue, black, cyan, cyan, black, record_color(record_toggle),	# first row
+	blue, blue, blue, black, cyan, cyan, black, black,				# first row
 	black, blue, black, black, cyan, black, cyan, black,				# second row
 	black, blue, black, black, cyan, black, cyan, black,				# third row
 	blue, blue, blue, black, cyan, cyan, black, black,				# fourth row
@@ -60,7 +47,7 @@ idle_led_pattern = [
 
 # lock mode pattern for 8x8 RGB LED array
 lock_led_pattern = [
-	blue, black, black, black, black, cyan, black, record_color(record_toggle),	# first row
+	blue, black, black, black, black, cyan, black, black,				# first row
 	blue, black, black, black, cyan, black, cyan, black,				# second row
 	blue, black, black, black, cyan, black, cyan, black,				# third row
 	blue, blue, blue, black, black, cyan, black, black,				# fourth row
@@ -72,7 +59,7 @@ lock_led_pattern = [
 
 # unlock mode pattern for 8x8 RGB LED array
 unlock_led_pattern = [
-	blue, black, blue, cyan, black, black, cyan, record_color(record_toggle),	# first row
+	blue, black, blue, cyan, black, black, cyan, black,				# first row
 	blue, black, blue, cyan, cyan, black, cyan, black,				# second row
 	blue, black, blue, cyan, black, cyan, cyan, black,				# third row
 	blue, blue, blue, cyan, black, black, cyan, black,				# fourth row
@@ -83,47 +70,61 @@ unlock_led_pattern = [
 ]
 
 
-
 def sigint_handler(signal_received, frame):
 
 	# print exit message when SIGINT/ctrl-c interrupt is caught
 	print("Exiting logger script")
+	sense.clear()
 	exit(0)
 
-def dislay_mode(mode):
-	return 0
+
+def display_mode(mode):
+
+	# switch statement to set current pattern for current mode
+	current_pattern = {
+		"unlock": unlock_led_pattern,
+		"lock"	:   lock_led_pattern,
+		"pick"	:   pick_led_pattern,
+		"idle"	:   idle_led_pattern
+	}[mode]
+
+	# set the current pattern on the 8x8 RGB LED array
+	sense.set_pixels(current_pattern)
+
 
 def log_orientation(mode):
 
-
-	# switch statement to set file path using mode parameter
-	"""
-	file_path = {
-		"unlock": "imu_unlock",
-		"lock"	: "imu_lock",
-		"pick"	: "imu_pick",
-		"idle"	: "imu_idle"
-	}[mode]
-	"""
 	# append start date and time of logging to file name/path
 	# (this prevents file from being rewritten on next function call)
 	file_path = "./log/" + mode + "/" + mode + "_" + datetime.now().strftime("%m_%d_%Y_%H_%M_%S") + ".csv"
 
+	# import the csv library
+	import csv
+
 	# open the file at the specified path with write permission
 	with open(file_path, 'w', newline='') as file:
 
-		import csv
+		# create a list that defines the header for the csv
+		csv_header = ['orientation', 'datetime', 'mode']
+
+
 		# create and instantiate a csv writer
 		writer = csv.writer(file)
-		while True:
 
+		# write the header to the csv file (once)
+		writer.writerow(csv_header)
+
+		while True:
 			#  create signal for SIGINT interrupt and handle it with sigint_handler function
 			signal(SIGINT, sigint_handler)
+
+			# display the current mode on the 8x8 RGB LED array
+			display_mode(mode)
 
 			# create data list variable and add orientation, current time, and mode
 			data = []
 			data.append(sense.get_orientation_degrees())
-			data.append(datetime.now())
+			data.append(datetime.now().strftime("%m_%d_%Y_%H_%M_%S"))
 			data.append(mode)
 
 			# write the data list to the csv file
@@ -132,23 +133,18 @@ def log_orientation(mode):
 			# print the data list (for debug)
 			print("\n".join(map(str, data)))
 
-			# break out of the logging loop if recording is turned off
-			if record_toggle == False:
-				break
-
-
-
-
-
 
 # main function
 if __name__ == "__main__":
 
-
 	# create and instantiate sense-hat variable
 	sense = SenseHat()
 
-	sense.set_pixels(unlock_led_pattern)
+	# check if the first argument is help
+	if argv[1] == "-help" or argv[1] == "help" or argv[1] == "-h":
+		print("		This script tracks the motion/orientation of the SenseHat, with different modes for tracking.")
+		print("		Current working modes: lock, unlock, idle, pick")
+	# take in first argument as the mode
+	else:
+		log_orientation(argv[1])
 
-	# log the orientation
-	log_orientation(current_mode)
